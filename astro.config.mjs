c47ddaf120f70cb6cb1dcb91da415e62837987c0
Chris <xlemperos@gmail.com>
2025-11-25 18:32:04 +0400

    minor fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    adapter: vercel(),
+    site: 'https://portfolio-9pvnkyw7p-christodoulos-lemperos-projects.vercel.app/', // optional, but good for SEO
+});
